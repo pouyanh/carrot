65916676a05ebc60e1fba4fc145949678a524896
Pouyan Heyratpour <pouyan@janstun.com>
2018-12-18 01:20:32 +0330

    Screen builder: use widget builders
    Signed-off-by: Pouyan Heyratpour <pouyan@janstun.com>

--- screens.lua
+++ screens.lua
@@ -15,6 +15,7 @@ local vicious = require("vicious") -- Vicious Widgets
 local freedesktop = require("freedesktop") -- Freedesktop.org menu and desktop icons support for Awesome WM
 
 local _t = awful.util.table or gears.table -- 4.{0,1} compatibility
+local markup = lain.util.markup
 
 -- {{{ Actions
 local function toggleClientMenu()
@@ -29,9 +30,92 @@ local function toggleClientMenu()
     end
   end
 end
+
+local function setScreenWallpaper(s)
+  -- Wallpaper
+  if beautiful.wallpaper then
+    local wallpaper = beautiful.wallpaper
+    -- If wallpaper is a function, call it with the screen
+    if type(wallpaper) == "function" then
+      wallpaper = wallpaper(s)
+    end
+    gears.wallpaper.maximized(wallpaper, s, true)
+  end
+end
 -- }}}
 
-local function newTaglistMouseBindings(modkey)
+-- {{{ Screen Setup
+-- Screen builder
+local function newScreenBuilder(tags, apps, builders)
+  -- Create a wibox for each screen and add it
+  return (function(s)
+    -- Each screen has its own tag table.
+    awful.tag(tags, s, awful.layout.layouts[1])
+
+    -- Unique per screen
+    s.mytaglist = builders.taglist(s)
+    s.mypromptbox = builders.promptbox(s)
+    s.mytasklist = builders.tasklist(s)
+    s.mylayoutbox = builders.layoutbox(s)
+
+    -- Common between screens
+    local mylauncher = builders.launcher(s)
+    local mykeyboardlayout = builders.kbdlayout(s)
+    local mybattery = builders.battery(s)
+    local mysystray = builders.systray(s)
+    local mytextclock = builders.clock(s)
+
+    -- Create the top wibox
+    s.mywibox = awful.wibar({
+      position = "top",
+      screen = s,
+    })
+
+    -- Add widgets to the wibox
+    s.mywibox:setup({
+      layout = wibox.layout.align.horizontal,
+      { -- Left widgets
+        layout = wibox.layout.fixed.horizontal,
+        mylauncher,
+        s.mytaglist,
+        s.mypromptbox,
+      },
+      s.mytasklist, -- Middle widget
+      { -- Right widgets
+        layout = wibox.layout.fixed.horizontal,
+        mykeyboardlayout,
+        mybattery,
+        mysystray,
+        mytextclock,
+        s.mylayoutbox,
+      },
+    })
+
+    -- Wallpaper
+    setScreenWallpaper(s)
+
+    -- Quake application
+    s.quake = lain.util.quake({ app = apps.terminal })
+
+    -- Let theme create wibox
+    -- beautiful.at_screen_connect(s)
+  end)
+end
+-- }}}
+
+-- {{{ Widget Builders
+-- Launcher
+local function newLauncherBuilder(menu)
+  return (function(s)
+    local mylauncher = awful.widget.launcher({
+      image = beautiful.awesome_icon,
+      menu = menu,
+    })
+  end)
+end
+
+-- Taglist
+local function newTaglistBuilder(modkey)
   local buttons = _t.join(
     awful.button({}, 1, function(t)
       t:view_only()
@@ -51,10 +135,20 @@ local function newTaglistMouseBindings(modkey)
     awful.button({}, 5, function(t) awful.tag.viewprev(t.screen) end)
   )
 
-  return buttons
+  return (function(s)
+    return awful.widget.taglist(s, awful.widget.taglist.filter.all, buttons)
+  end)
 end
 
-local function newTasklistMouseBindings()
+-- Promptbox
+local function newPromptboxBuilder()
+  return (function(s)
+    return awful.widget.prompt()
+  end)
+end
+
+-- Tasklist
+local function newTasklistBuilder()
   local buttons = _t.join(
     -- Left mouse button: minimize/restore
     awful.button({}, 1, function (c)
@@ -93,127 +187,105 @@ local function newTasklistMouseBindings()
     end)
   )
 
-  return buttons
+  return (function(s)
+    return awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, buttons)
+  end)
 end
-
-local function setScreenWallpaper(s)
-  -- Wallpaper
-  if beautiful.wallpaper then
-    local wallpaper = beautiful.wallpaper
-    -- If wallpaper is a function, call it with the screen
-    if type(wallpaper) == "function" then
-      wallpaper = wallpaper(s)
-    end
-    gears.wallpaper.maximized(wallpaper, s, true)
-  end
-end
-
--- {{{ Screen Setup
-
--- Screen builder
-local function newScreenBuilder(tags, taglistButtons, tasklistButtons, apps)
-  -- Create a wibox for each screen and add it
-  return function(s)
-    -- Quake application
-    s.quake = lain.util.quake({ app = apps.terminal })
-
-    -- Each screen has its own tag table.
-    awful.tag(tags, s, awful.layout.layouts[1])
-
-    -- Create a taglist widget
-    s.mytaglist = awful.widget.taglist(s, awful.widget.taglist.filter.all, taglistButtons)
-
-    -- Create a promptbox for each screen
-    s.mypromptbox = awful.widget.prompt()
-
-    -- Create a tasklist widget
-    s.mytasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, tasklistButtons)
-
-    -- Create an imagebox widget which will contains an icon indicating which layout we're using.
-    -- We need one layoutbox per screen.
-    s.mylayoutbox = awful.widget.layoutbox(s)
-    s.mylayoutbox:buttons(_t.join(
-      awful.button({}, 1, function () awful.layout.inc(1) end),
-      awful.button({}, 3, function () awful.layout.inc(-1) end),
-      awful.button({}, 4, function () awful.layout.inc(1) end),
-      awful.button({}, 5, function () awful.layout.inc(-1) end)
-    ))
-
-    -- Create the wibox
-    s.mywibox = awful.wibar({
-      position = "top",
-      screen = s,
-    })
-
-    -- Add widgets to the wibox
-    s.mywibox:setup({
-      layout = wibox.layout.align.horizontal,
-      { -- Left widgets
-        layout = wibox.layout.fixed.horizontal,
-        mylauncher,
-        s.mytaglist,
-        s.mypromptbox,
-      },
-      s.mytasklist, -- Middle widget
-      { -- Right widgets
-        layout = wibox.layout.fixed.horizontal,
-        mykeyboardlayout,
-        wibox.widget.systray(),
-        mytextclock,
-        s.mylayoutbox,
-      },
-    })
-
-    -- Wallpaper
-    setScreenWallpaper(s)
-
-    -- Let theme create wibox
-    -- beautiful.at_screen_connect(s)
-  end
-end
--- }}}
-
--- {{{ Widgets
--- Launcher
-local mylauncher = awful.widget.launcher({
-  image = beautiful.awesome_icon,
-  --menu = mymainmenu
-})
 
 -- Keyboard map indicator and switcher
-local mykeyboardlayout = awful.widget.keyboardlayout()
+local function newKeyboardLayoutBuilder()
+  return (function(s)
+    return awful.widget.keyboardlayout()
+  end)
+end
+
+-- Battery status indicator
+local function newBatteryBuilder()
+  return (function(s)
+    local battery = lain.widget.bat({
+      settings = (function()
+        local perc = bat_now.perc ~= "N/A" and bat_now.perc .. "%" or bat_now.perc
+
+        if bat_now.ac_status == 1 then
+          perc = perc .. " plug"
+        end
+
+        widget:set_markup(markup.fontfg("xos4 Terminus 8", "#aaaaaa", perc .. " "))
+      end)
+    })
+
+    return battery.widget
+  end)
+end
+
+-- Systray
+local function newSystrayBuilder()
+  return (function(s)
+    return wibox.widget.systray()
+  end)
+end
 
 -- Create a textclock widget
-local mytextclock = wibox.widget.textclock()
+local function newClockBuilder()
+  return (function(s)
+    local clock = wibox.widget.textclock(markup("#7788af", "%A %d %B ") .. markup("#ab7367", ">") .. markup("#de5e1e", " %H:%M "))
 
--- Battery Widget
-batwidget = awful.widget.progressbar()
-batwidget:set_width(8)
-batwidget:set_height(10)
-batwidget:set_vertical(true)
-batwidget:set_background_color("#494B4F")
-batwidget:set_border_color(nil)
-batwidget:set_color("#AECF96")
-batwidget:set_color({ type = "linear", from = { 0, 0 }, to = { 0, 10 },
-stops = {{ 0, "#AECF96" }, { 0.5, "#88A175" }, { 1, "#FF5656" }}})
-vicious.register(batwidget, vicious.widgets.bat, "$2", 61, "BAT0")
+    -- Calendar
+    clock.calendar = lain.widget.cal({
+      attach_to = { clock },
+      notification_preset = {
+        font = "xos4 Terminus 10",
+        -- fg = theme.fg_normal,
+        -- bg = theme.bg_normal,
+        fg = "#aaaaaa",
+        bg = "#000000",
+      }
+    })
+
+    return clock
+  end)
+end
+
+-- Layoutbox
+local function newLayoutboxBuilder()
+  local buttons = _t.join(
+    awful.button({}, 1, function () awful.layout.inc(1) end),
+    awful.button({}, 3, function () awful.layout.inc(-1) end),
+    awful.button({}, 4, function () awful.layout.inc(1) end),
+    awful.button({}, 5, function () awful.layout.inc(-1) end)
+  )
+
+  return (function(s)
+    -- Create an imagebox widget which will contains an icon indicating which layout we're using.
+    -- We need one layoutbox per screen.
+    local lb = awful.widget.layoutbox(s)
+    lb:buttons(buttons)
+
+    return lb
+  end)
+end
 -- }}}
 
-
 local function setup(config)
-  local tasklistButtons = newTasklistMouseBindings()
-  local taglistButtons = newTaglistMouseBindings(config.mod)
+  -- Builders
+  local builders = {
+    launcher = newLauncherBuilder();
+    taglist = newTaglistBuilder(config.mod);
+    promptbox = newPromptboxBuilder();
+    tasklist = newTasklistBuilder();
+    kbdlayout = newKeyboardLayoutBuilder();
+    battery = newBatteryBuilder();
+    systray = newSystrayBuilder();
+    clock = newClockBuilder();
+    layoutbox = newLayoutboxBuilder();
+  }
+  local screenBuilder = newScreenBuilder(config.tags, config.apps, builders)
 
   -- awful.layout.layouts = ???
 
   -- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
   screen.connect_signal("property::geometry", setScreenWallpaper)
-  awful.screen.connect_for_each_screen(newScreenBuilder(
-    config.tags,
-    taglistButtons,
-    tasklistButtons,
-    config.apps
-  ))
+  awful.screen.connect_for_each_screen(screenBuilder)
 end
 
 return {
